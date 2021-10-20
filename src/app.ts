a9fcd47dd6596e081a1776bc4f25fb12dce4fe15
Bruno Leitao Donatelli <bleitaod@everis.com>
2021-10-20 16:21:50 -0300

    numbers, args and functions

--- src/app.ts
+++ src/app.ts
@@ -3,65 +3,103 @@ console.log('Hello Typescript');
 
 // Part 2 ES6/7:
 // Arrow function and implicit return
-const pizzas = [
-    { name: 'Peperoni', topics: ['peperoni'] }
-];
+function arrowFunctionAndImplicitReturn(): void {
+    const pizzas = [
+        { name: 'Peperoni', topics: ['peperoni'] }
+    ];
 
-pizzas.map(pizza => pizza.name.toUpperCase());
+    pizzas.map(pizza => pizza.name.toUpperCase());
 
-console.log(pizzas);
+    console.log(pizzas);
+}
 
 // object literals
+function objectLiterals(): void {
+    const pizza = {
+        name: 'pepperoni',
+        price: 15,
+        getName() {
+            return this.name;
+        }
+        /*if u want to use arrow instead conventional function, remember the following thing: arrow function doesn't bind this context, so you want to use the object name
+        
+        getName: () => {
+            return pizza.name;
+        }
+        */
+    }
+    const toppings = ['pepperoni'];
 
-const pits = {
-    name: 'pepperoni',
-    price: 15,
-    getName() {
-        return this.name;
-    }
-    /*if u want to use arrow instead conventional function, remember the following thing: arrow function doesn't bind this context, so you want to use the object name
-    
-    getName: () => {
-        return pizza.name;
-    }
-    */
+    const order = {
+        pizza,
+        toppings
+    };
+    console.log(order);
 }
-const toppings = ['pepperoni'];
-
-const order = {
-    pits,
-    toppings
-};
-console.log(order);
 
 
 // rest operator
+function restOperator(): void {
+    function sumAll(message: string, ...arr: number[]) {
+        console.log(arguments); // this reserved word show us the object map
+        return arr.reduce((prev, next) => prev + next);
+    }
 
-function sumAll(message: string, ...arr: number[]) {
-    console.log(arguments); // this reserved word show us the object map
-    return arr.reduce((prev, next) => prev + next);
+    sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
 }
-
-sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
 // array spread
 
-const toppingss = ['bacon', 'chilli'];
-const newToppingss = ['Margherita'];
+function arraySpread(): void {
+    const toppings = ['bacon', 'chilli'];
+    const newToppingss = ['Margherita'];
 
-/** how to merge both of arrays? */
-const allToppings = [...toppings, ...newToppingss]; // kep the references of all arrays
-// toppings.concat(newToppingss);
+    /** how to merge both of arrays? */
+    const allToppings = [...toppings, ...newToppingss]; // kep the references of all arrays
+    // toppings.concat(newToppingss);
 
-// object spread operator
-const pizzza = { name: 'margherita' };
-const toppingsPizza = ['pepperoni'];
-const orderPizzza = {
-    ...pizzza,
-    toppingsPizza
+    // object spread operator
+    const pizza = { name: 'margherita' };
+    const toppingsPizza = ['pepperoni'];
+    const orderPizza = {
+        ...pizza,
+        toppingsPizza
+    }
+    /**
+     * this copy also can be:
+     * 
+     * var newOrderPizzza = Object.assign({}, pizzza, {toppingsPizzza});
+     */
 }
-/**
- * this copy also can be:
- * 
- * var newOrderPizzza = Object.assign({}, pizzza, {toppingsPizzza});
- */
+
+// destructuring object and array
+function destructuring(): void {
+    const piza = {
+        name: 'margherita',
+        toppings: ['margherita'],
+    };
+
+    function orderPiza({ name: pizzaName, toppings: pizzaToppics }: typeof piza): void {
+        console.log(pizzaName, pizzaToppics);
+    }
+
+    const { name: pizzaName } = piza;
+
+    const toppings2 = ['rice', 'bean'];
+    const [first, second] = toppings2;
+
+}
+
+// number, arguments and functions
+function numberTypes(): void {
+    const pizzaCost: number = 10;
+    const pizzaToppings: number = 5;
+
+    function calculatePrice(cost: number, toppings: number): number {
+        return cost + 1.5 * toppings;
+    }
+
+    const cost: number = calculatePrice(pizzaCost, pizzaToppings);
+    console.log(`Pizza costs ${cost}`);
+}
