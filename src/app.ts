b5a2e696611037781e41db2abf776d7108eccddb
Bruno Leitao Donatelli <bleitaod@everis.com>
2021-10-25 11:00:35 -0300

    adding classes instead pure functional programming

--- src/app.ts
+++ src/app.ts
@@ -3,92 +3,100 @@ console.log('Hello Typescript');
 
 // Part 2 ES6/7:
 // Arrow function and implicit return
-function arrowFunctionAndImplicitReturn(): void {
-    const pizzas = [
-        { name: 'Peperoni', topics: ['peperoni'] }
-    ];
+class ArrowFunction {
+    arrowFunctionAndImplicitReturn(): void {
+        const pizzas = [
+            { name: 'Peperoni', topics: ['peperoni'] }
+        ];
 
-    pizzas.map(pizza => pizza.name.toUpperCase());
+        pizzas.map(pizza => pizza.name.toUpperCase());
 
-    console.log(pizzas);
+        console.log(pizzas);
+    }
 }
 
 // object literals
-function objectLiterals(): void {
-    const pizza = {
-        name: 'pepperoni',
-        price: 15,
-        getName() {
-            return this.name;
+class ObjectLiterals {
+    objectLiterals(): void {
+        const pizza = {
+            name: 'pepperoni',
+            price: 15,
+            getName() {
+                return this.name;
+            }
+            /*if u want to use arrow instead conventional function, remember the following thing: arrow function doesn't bind this context, so you want to use the object name
+            
+            getName: () => {
+                return pizza.name;
+            }
+            */
         }
-        /*if u want to use arrow instead conventional function, remember the following thing: arrow function doesn't bind this context, so you want to use the object name
-        
-        getName: () => {
-            return pizza.name;
-        }
-        */
+        const toppings = ['pepperoni'];
+
+        const order = {
+            pizza,
+            toppings
+        };
+        console.log(order);
     }
-    const toppings = ['pepperoni'];
-
-    const order = {
-        pizza,
-        toppings
-    };
-    console.log(order);
 }
-
 
 // rest operator
-function restOperator(): void {
-    function sumAll(message: string, ...arr: number[]) {
-        console.log(arguments); // this reserved word show us the object map
-        return arr.reduce((prev, next) => prev + next);
+class RestSpreadOperator {
+
+    restOperator(): void {
+        function sumAll(message: string, ...arr: number[]) {
+            console.log(arguments); // this reserved word show us the object map
+            return arr.reduce((prev, next) => prev + next);
+        }
+
+        sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
     }
 
-    sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    // array spread
 
+    arraySpread(): void {
+        const toppings = ['bacon', 'chilli'];
+        const newToppingss = ['Margherita'];
+
+        /** how to merge both of arrays? */
+        const allToppings = [...toppings, ...newToppingss]; // kep the references of all arrays
+        // toppings.concat(newToppingss);
+
+        // object spread operator
+        const pizza = { name: 'margherita' };
+        const toppingsPizza = ['pepperoni'];
+        const orderPizza = {
+            ...pizza,
+            toppingsPizza
+        }
+        /**
+         * this copy also can be:
+         * 
+         * var newOrderPizzza = Object.assign({}, pizzza, {toppingsPizzza});
+         */
+    }
 }
 
-// array spread
+class Destructuring {
+    // destructuring object and array
+    destructuring(): void {
+        const pizza = {
+            name: 'margherita',
+            toppings: ['margherita'],
+        };
 
-function arraySpread(): void {
-    const toppings = ['bacon', 'chilli'];
-    const newToppingss = ['Margherita'];
+        function orderPiza({ name: pizzaName, toppings: pizzaToppics }: typeof pizza): void {
+            console.log(pizzaName, pizzaToppics);
+        }
 
-    /** how to merge both of arrays? */
-    const allToppings = [...toppings, ...newToppingss]; // kep the references of all arrays
-    // toppings.concat(newToppingss);
+        const { name: pizzaName } = pizza;
 
-    // object spread operator
-    const pizza = { name: 'margherita' };
-    const toppingsPizza = ['pepperoni'];
-    const orderPizza = {
-        ...pizza,
-        toppingsPizza
+        const toppings2 = ['rice', 'bean'];
+        const [first, second] = toppings2;
+
     }
-    /**
-     * this copy also can be:
-     * 
-     * var newOrderPizzza = Object.assign({}, pizzza, {toppingsPizzza});
-     */
-}
-
-// destructuring object and array
-function destructuring(): void {
-    const piza = {
-        name: 'margherita',
-        toppings: ['margherita'],
-    };
-
-    function orderPiza({ name: pizzaName, toppings: pizzaToppics }: typeof piza): void {
-        console.log(pizzaName, pizzaToppics);
-    }
-
-    const { name: pizzaName } = piza;
-
-    const toppings2 = ['rice', 'bean'];
-    const [first, second] = toppings2;
-
 }
 
 // number, arguments and functions
@@ -298,41 +306,19 @@ class DivingIntoInterfaces {
         interface Pizza {
             name: string;
             sizes: string[];
+            getAvailableSizes(): string[];
         }
 
         let pizza: Pizza;
 
         function createPizza(name: string, sizes: string[]): Pizza {
             return {
-                name, sizes
+                name,
+                sizes,
+                getAvailableSizes() { return this.sizes }
             };
         }
 
         pizza = createPizza('Margherita', ['small', 'medium']);
     }
 }
-
-(async () => {
-    // neverType(),
-    await Promise.all([
-        arraySpread(),
-        arrowFunctionAndImplicitReturn(),
-        destructuring(),
-        numberTypes(),
-        objectLiterals(),
-        restOperator(),
-        stringTypes(),
-        booleanType(),
-        anyType(),
-        implicitVsExplicitTypes(),
-        voidType(),
-        unionTypesAndLiteralTypes(),
-        functionTypes(),
-        functionTypesWithOptionals(),
-        typedFunction(),
-        objectType(),
-        arrayTypesAndGenerics(),
-        typeAlias(),
-        typeAssertions()
-    ]);
-})();
