0716c4f022b241432a6aacdfc7dc1835de4d6576
Bruno Leitao Donatelli <bleitaod@emeal.nttdata.com>
2021-11-24 14:58:27 -0300

    started intersection types

--- uc-typescript-masterclass/src/guardtypes/instanceDefinedAndLiteral.ts
+++ uc-typescript-masterclass/src/guardtypes/instanceDefinedAndLiteral.ts
@@ -1,26 +1,21 @@
-class Foo {
-    bar() { }
-}
-
-const bar = new Foo();
-console.log(Object.getPrototypeOf(bar));
-
-
 class Song {
+    kind: 'song';
     constructor(public title: string, public duration: number) { }
-
 }
 
 class Playlist {
+    kind: 'playlist';
+
     constructor(public name: string, public songs: Song[]) { }
 }
 
 function isSong(item): item is Song {
-    return item instanceof Song;
+    return 'title' in item;
 }
 
 function getItemName(item: Song | Playlist) {
-    if (isSong(item)) {
+    // if (isSong(item)) {
+    if (item.kind === 'song') {
         return item.title;
     }
     return item.name;
