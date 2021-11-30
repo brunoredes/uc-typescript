aa942fbb33de8781407fc70d988c3966606d0827
Bruno Leitao Donatelli <bleitaod@emeal.nttdata.com>
2021-11-30 16:33:51 -0300

    classVsInterface

--- uc-typescript-masterclass/src/intersectionTypes/discriminatedTypes.ts
+++ uc-typescript-masterclass/src/intersectionTypes/discriminatedTypes.ts
@@ -1,66 +1,69 @@
-interface Order {
-    id: string;
-    amount: number;
-    currency: string;
-}
+function discriminatedTypes() {
 
-interface Stripe {
-    type: 'stripe';
-    card: string;
-    cvc: string;
-}
-
-interface Paypal {
-    type: 'paypal';
-    email: string;
-}
-
-interface PicPay {
-    type: 'picpay';
-    telephoneNumber: string
-}
-
-type CheckoutCard = Order & Stripe;
-type CheckoutPaypal = Order & Paypal;
-type CheckoutPicpay = Order & PicPay;
-
-const order: Order = {
-    id: 'xa22s',
-    amount: 100,
-    currency: 'USD'
-};
-
-const orderCard: CheckoutCard = {
-    ...order,
-    card: '1000 2000 3000 4000',
-    cvc: '123',
-    type: 'stripe'
-};
-
-const orderPaypal: CheckoutPaypal = {
-    ...order,
-    email: 'abc@def.com',
-    type: 'paypal'
-
-};
-
-const orderPicpay: CheckoutPicpay = {
-    ...order,
-    type: 'picpay',
-    telephoneNumber: '+551199999999',
-
-}
-
-type Payload = CheckoutCard | CheckoutPaypal | CheckoutPicpay;
-
-function checkout(payload: Payload) {
-    if (payload.type === 'stripe') {
-        console.log(payload.card, payload.cvc)
+    interface Order {
+        id: string;
+        amount: number;
+        currency: string;
     }
-    if (payload.type === 'picpay') {
-        console.log(payload.telephoneNumber);
+
+    interface Stripe {
+        type: 'stripe';
+        card: string;
+        cvc: string;
     }
-    if (payload.type === 'paypal') {
-        console.log(payload.email);
+
+    interface Paypal {
+        type: 'paypal';
+        email: string;
+    }
+
+    interface PicPay {
+        type: 'picpay';
+        telephoneNumber: string
+    }
+
+    type CheckoutCard = Order & Stripe;
+    type CheckoutPaypal = Order & Paypal;
+    type CheckoutPicpay = Order & PicPay;
+
+    const order: Order = {
+        id: 'xa22s',
+        amount: 100,
+        currency: 'USD'
+    };
+
+    const orderCard: CheckoutCard = {
+        ...order,
+        card: '1000 2000 3000 4000',
+        cvc: '123',
+        type: 'stripe'
+    };
+
+    const orderPaypal: CheckoutPaypal = {
+        ...order,
+        email: 'abc@def.com',
+        type: 'paypal'
+
+    };
+
+    const orderPicpay: CheckoutPicpay = {
+        ...order,
+        type: 'picpay',
+        telephoneNumber: '+551199999999',
+
+    }
+
+    type Payload = CheckoutCard | CheckoutPaypal | CheckoutPicpay;
+
+    function checkout(payload: Payload) {
+        if (payload.type === 'stripe') {
+            console.log(payload.card, payload.cvc)
+        }
+        if (payload.type === 'picpay') {
+            console.log(payload.telephoneNumber);
+        }
+        if (payload.type === 'paypal') {
+            console.log(payload.email);
+        }
     }
 }
