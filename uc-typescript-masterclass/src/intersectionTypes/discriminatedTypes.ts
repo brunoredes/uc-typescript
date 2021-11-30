9752879baee1ef4e7b10cc8ac4dcb7f05a6f5ae9
Bruno Leitao Donatelli <bleitaod@emeal.nttdata.com>
2021-11-30 15:16:02 -0300

    discriminatedTypes and interfaceVsTypeAliases

---
+++ uc-typescript-masterclass/src/intersectionTypes/discriminatedTypes.ts
@@ -0,0 +1,66 @@
+interface Order {
+    id: string;
+    amount: number;
+    currency: string;
+}
+
+interface Stripe {
+    type: 'stripe';
+    card: string;
+    cvc: string;
+}
+
+interface Paypal {
+    type: 'paypal';
+    email: string;
+}
+
+interface PicPay {
+    type: 'picpay';
+    telephoneNumber: string
+}
+
+type CheckoutCard = Order & Stripe;
+type CheckoutPaypal = Order & Paypal;
+type CheckoutPicpay = Order & PicPay;
+
+const order: Order = {
+    id: 'xa22s',
+    amount: 100,
+    currency: 'USD'
+};
+
+const orderCard: CheckoutCard = {
+    ...order,
+    card: '1000 2000 3000 4000',
+    cvc: '123',
+    type: 'stripe'
+};
+
+const orderPaypal: CheckoutPaypal = {
+    ...order,
+    email: 'abc@def.com',
+    type: 'paypal'
+
+};
+
+const orderPicpay: CheckoutPicpay = {
+    ...order,
+    type: 'picpay',
+    telephoneNumber: '+551199999999',
+
+}
+
+type Payload = CheckoutCard | CheckoutPaypal | CheckoutPicpay;
+
+function checkout(payload: Payload) {
+    if (payload.type === 'stripe') {
+        console.log(payload.card, payload.cvc)
+    }
+    if (payload.type === 'picpay') {
+        console.log(payload.telephoneNumber);
+    }
+    if (payload.type === 'paypal') {
+        console.log(payload.email);
+    }
+}
--- uc-typescript-masterclass/src/intersectionTypes/intersectionType.ts
+++ uc-typescript-masterclass/src/intersectionTypes/intersectionType.ts
@@ -1,37 +1,49 @@
-interface Order {
-    id: string;
-    amount: number;
-    currency: string;
+function intersectionTypes() {
+    interface Order {
+        id: string;
+        amount: number;
+        currency: string;
+    }
+
+    interface Stripe {
+        card: string;
+        cvc: string;
+    }
+
+    interface Paypal {
+        email: string;
+    }
+
+    interface PicPay {
+        telephoneNumber: string
+    }
+
+    type CheckoutCard = Order & Stripe;
+    type CheckoutPaypal = Order & Paypal;
+    type CheckoutPicpay = Order & PicPay;
+    // type CheckoutABC = Order & { name: string; };
+
+    const order: Order = {
+        id: 'xa22s',
+        amount: 100,
+        currency: 'USD'
+    };
+
+    const orderCard: CheckoutCard = {
+        ...order,
+        card: '1000 2000 3000 4000',
+        cvc: '123'
+    };
+
+    const orderPaypal: CheckoutPaypal = {
+        ...order,
+        email: 'abc@def.com'
+    };
+
+    const orderPicpay: CheckoutPicpay = {
+        ...order,
+        telephoneNumber: '+551199999999'
+    }
+
+    const assigned = Object.assign({}, order, order);
 }
-
-interface Stripe {
-    card: string;
-    cvc: string;
-}
-
-interface Paypal {
-    email: string;
-}
-
-type CheckoutCard = Order & Stripe;
-type CheckoutPaypal = Order & Paypal;
-// type CheckoutABC = Order & { name: string; };
-
-const order: Order = {
-    id: 'xa22s',
-    amount: 100,
-    currency: 'USD'
-};
-
-const orderCard: CheckoutCard = {
-    ...order,
-    card: '1000 2000 3000 4000',
-    cvc: '123'
-};
-
-const orderPaypal: CheckoutPaypal = {
-    ...order,
-    email: 'abc@def.com'
-};
-
-const assigned = Object.assign({}, order, order);
