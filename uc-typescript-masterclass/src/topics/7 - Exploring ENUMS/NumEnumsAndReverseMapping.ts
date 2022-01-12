59b5ad492b63a5f32c97370040865443ac73898f
bruno donatelli <bruno.donatelli@etec.sp.gov.br>
2022-01-12 18:34:05 -0300

    finished

--- uc-typescript-masterclass/src/topics/7 - Exploring ENUMS/NumEnumsAndReverseMapping.ts
+++ uc-typescript-masterclass/src/topics/7 - Exploring ENUMS/NumEnumsAndReverseMapping.ts
@@ -1,11 +1,35 @@
-enum Sizes {
-    'SMALL',
-    'MEDIUM',
-    'LARGE'
-}
+function numericalEnumsAndReverseMapping() {
+    enum Sizes {
+        'SMALL',
+        'MEDIUM',
+        'LARGE'
+    }
 
-enum Sizes {
-    'EXTRALARGE' = 3
-}
+    enum Sizes {
+        'EXTRALARGE' = 3
+    }
 
-console.log({sizePosition: Sizes.LARGE, stringValue: Sizes[2]});
+    console.log({ sizePosition: Sizes.LARGE, stringValue: Sizes[2] });
+};
+
+function stringEnumAndInliningMember() {
+    enum Sizes {
+        SMALL = 'SMALL',
+        MEDIUM = 'MEDIUM',
+        LARGE = 'LARGE'
+    }
+
+    const enum Tamanhos {
+        PEQUENO = 'PEQUENO',
+        MEDIO = 'MEDIO',
+        GRANDE = 'GRANDE'
+    }
+
+    let selected: Tamanhos = Tamanhos.PEQUENO;
+
+    function updateSize(size: Tamanhos): void {
+        selected = size;
+    }
+
+    updateSize(Tamanhos.GRANDE);
+}
